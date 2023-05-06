e81781d1c0d8000b97f3c72e73c7c4074ded73f6
Abdulla Farooq <voilad8a@gmail.com>
2023-05-07 01:28:54 +0300

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "white",
+        secondary: "gray-900",
+        accent: "#FF1E56",
+      },
+    },
+  },
+  plugins: [],
+};
