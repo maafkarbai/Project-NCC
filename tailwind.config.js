1c9d62f59776c0fe28f7fd5fd686744dcc326a45
Abdulla Farooq <voilad8a@gmail.com>
2023-05-17 02:26:11 +0300

    new file:   GWSC.ico 	new file:   LOGO.ico 	new file:   Logo.jpg 	modified:   index.html 	modified:   style.css 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
-      colors: {
-        primary: "white",
-        secondary: "gray-900",
-        accent: "#FF1E56",
-      },
+      secondary: "white",
     },
   },
   plugins: [],
